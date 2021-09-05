444293c89436cfc3beae036dbc3ee50524c6588b
Stefan Benten <mail@stefan-benten.de>
2021-09-05 10:50:36 +0200

    User ticker instead of timer
    Signed-off-by: Stefan Benten <mail@stefan-benten.de>

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -35,39 +35,51 @@ func NewPowerFoxClient() {
 	user := os.Getenv("PF_USER")
 	pass := os.Getenv("PF_PASS")
 
-	timer := time.NewTimer(pollInterval)
+	timer := time.NewTicker(pollInterval)
+	done := make(chan bool)
 
+	// Fire of go routine to handle polling.
 	go func() {
-		<-timer.C
+		for {
+			select {
+			case <-done:
+				return
+			case <-timer.C:
 
-		client := &http.Client{}
-		var powerFoxData = PowerFoxStatus{}
+				client := &http.Client{}
+				var powerFoxData = PowerFoxStatus{}
 
-		req, err := http.NewRequest("GET", powerFoxUrl, nil)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
+				req, err := http.NewRequest("GET", powerFoxUrl, nil)
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+
+				req.SetBasicAuth(user, pass)
+				req.Header.Set("Accept", "application/json")
+
+				resp, err := client.Do(req)
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				defer resp.Body.Close()
+
+				data, err := io.ReadAll(resp.Body)
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+
+				err = json.Unmarshal(data, &powerFoxData)
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+
+				fmt.Println(powerFoxData)
+			}
 		}
-
-		req.SetBasicAuth(user, pass)
-		req.Header.Set("Accept", "application/json")
-
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-		defer resp.Body.Close()
-
-		data, err := io.ReadAll(resp.Body)
-
-		err = json.Unmarshal(data, &powerFoxData)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		fmt.Println(powerFoxData)
 	}()
 }
 
